cb561736d943860a669caa33c8ddd659747e0c09
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-08-31 15:56:52 +0530

    validation the currentForeignReservesData adding

--- src/main/java/com/example/l010myprojectsworldeconomyindex/repository/CurrentForeignReservesRepository.java
+++ src/main/java/com/example/l010myprojectsworldeconomyindex/repository/CurrentForeignReservesRepository.java
@@ -2,9 +2,14 @@ package com.example.l010myprojectsworldeconomyindex.repository;
 
 import com.example.l010myprojectsworldeconomyindex.model.CurrentForeignReserves;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CurrentForeignReservesRepository extends JpaRepository<CurrentForeignReserves, Long> {
 
+    @Query("SELECT c from CurrentForeignReserves c where c.country.countryName = ?1")
+    public Optional<CurrentForeignReserves> findCurrentForeignReservesByCountryName(String countryName);
 }
--- src/main/java/com/example/l010myprojectsworldeconomyindex/service/CurrentForeignReservesService.java
+++ src/main/java/com/example/l010myprojectsworldeconomyindex/service/CurrentForeignReservesService.java
@@ -5,6 +5,7 @@ import com.example.l010myprojectsworldeconomyindex.repository.CurrentForeignRese
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CurrentForeignReservesService {
@@ -16,6 +17,20 @@ public class CurrentForeignReservesService {
     }
 
     public void addNewCurrentForeignReservesData(CurrentForeignReserves currentForeignReserves) {
+        Optional<CurrentForeignReserves> currentForeignReservesOptional = currentForeignReservesRepository.findCurrentForeignReservesByCountryName(currentForeignReserves.getCountry().getCountryName());
+
+        if (currentForeignReservesOptional.isPresent()) {
+            throw new IllegalStateException("country : " + currentForeignReserves.getCountry().getCountryName() + " is existing in the table, so update it");
+        } else if (currentForeignReserves.getCurrentForeignReservesValue().intValue() != currentForeignReserves.getForeignReserves().getForeignReservesValue()) {
+            throw new IllegalStateException("currentForeignReservesValue : " + currentForeignReserves.getCurrentForeignReservesValue() + " and foreignReservesValue : " + currentForeignReserves.getForeignReserves().getForeignReservesValue() + " does not same!");
+        } else if (currentForeignReserves.getCountry().getCountryId().intValue() != currentForeignReserves.getForeignReserves().getCountry().getCountryId()) {
+            throw new IllegalStateException("country in CurrentForeignReserves : " + currentForeignReserves.getCountry().getCountryId().intValue() + " and country in ForeignReserves : " + currentForeignReserves.getForeignReserves().getCountry().getCountryId() + " does not same!");
+        } else if (!currentForeignReserves.getYear().toString().equals(currentForeignReserves.getForeignReserves().getYear().toString())) {
+            throw new IllegalStateException("year in CurrentForeignReserves : " + currentForeignReserves.getYear() + "  and year in ForeignReserves : " + currentForeignReserves.getForeignReserves().getYear() + " does not same!");
+        } else if (currentForeignReserves.getMonth() != currentForeignReserves.getForeignReserves().getMonth()) {
+            throw new IllegalStateException("month in CurrentForeignReserves : " + currentForeignReserves.getMonth() + "  and month in ForeignReserves : " + currentForeignReserves.getForeignReserves().getMonth() + " does not same!");
+        }
+
         currentForeignReservesRepository.save(currentForeignReserves);
     }
 
