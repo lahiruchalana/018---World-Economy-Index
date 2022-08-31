195af3e0e31652434965bc19055e9ea7a4be7524
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-08-31 15:30:19 +0530

    GET and POST methods for CurrentForeignReserce Data

--- src/main/java/com/example/l010myprojectsworldeconomyindex/controller/CurrentForeignReservesController.java
+++ src/main/java/com/example/l010myprojectsworldeconomyindex/controller/CurrentForeignReservesController.java
@@ -1,4 +1,33 @@
 package com.example.l010myprojectsworldeconomyindex.controller;
 
+import com.example.l010myprojectsworldeconomyindex.model.CurrentForeignReserves;
+import com.example.l010myprojectsworldeconomyindex.service.CurrentForeignReservesService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/data/current/foreign/reserves")
 public class CurrentForeignReservesController {
+
+    private final CurrentForeignReservesService currentForeignReservesService;
+
+    public CurrentForeignReservesController(CurrentForeignReservesService currentForeignReservesService) {
+        this.currentForeignReservesService = currentForeignReservesService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addNewCurrentForeignReservesData(
+            @RequestBody CurrentForeignReserves currentForeignReserves
+    ) {
+        currentForeignReservesService.addNewCurrentForeignReservesData(currentForeignReserves);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CurrentForeignReserves>> getAllCurrentForeignReservesData() {
+        return new ResponseEntity<>(currentForeignReservesService.getAllCurrentForeignReservesData(), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/l010myprojectsworldeconomyindex/service/CurrentForeignReservesService.java
+++ src/main/java/com/example/l010myprojectsworldeconomyindex/service/CurrentForeignReservesService.java
@@ -1,4 +1,25 @@
 package com.example.l010myprojectsworldeconomyindex.service;
 
+import com.example.l010myprojectsworldeconomyindex.model.CurrentForeignReserves;
+import com.example.l010myprojectsworldeconomyindex.repository.CurrentForeignReservesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CurrentForeignReservesService {
+
+    private final CurrentForeignReservesRepository currentForeignReservesRepository;
+
+    public CurrentForeignReservesService(CurrentForeignReservesRepository currentForeignReservesRepository) {
+        this.currentForeignReservesRepository = currentForeignReservesRepository;
+    }
+
+    public void addNewCurrentForeignReservesData(CurrentForeignReserves currentForeignReserves) {
+        currentForeignReservesRepository.save(currentForeignReserves);
+    }
+
+    public List<CurrentForeignReserves> getAllCurrentForeignReservesData() {
+        return currentForeignReservesRepository.findAll();
+    }
 }
