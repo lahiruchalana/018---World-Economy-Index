beca5233e8e7f0730b95eed60f6c97b9f47d1a29
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-09-01 15:29:32 +0530

    ManyToMany Relationship with currency and country tbl

--- src/main/java/com/example/l010myprojectsworldeconomyindex/model/Currency.java
+++ src/main/java/com/example/l010myprojectsworldeconomyindex/model/Currency.java
@@ -1,4 +1,48 @@
 package com.example.l010myprojectsworldeconomyindex.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "currency_tbl")
 public class Currency {
+
+    @Id
+    @SequenceGenerator(
+            name = "currency_sequence",
+            sequenceName = "currency_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "currency_sequence"
+    )
+    private Long currencyId;
+    private String currencyName;
+
+    @ManyToMany(
+            fetch = FetchType.EAGER
+    )
+    @JoinTable(
+            name = "currency_country_tbl",
+            joinColumns = @JoinColumn(
+                    name = "currency_id",
+                    referencedColumnName = "currencyId"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "country_id",
+                    referencedColumnName = "countryId"
+            )
+    )
+    private List<Country> countryList;
+
+    public Currency() {
+    }
+
+    public Currency(String currencyName, List<Country> countryList) {
+        this.currencyName = currencyName;
+        this.countryList = countryList;
+    }
+
+
 }
